3c2c83128c453aec385db82a2175a80f9867eb4a
Karadesh <akamalavr@gmail.com>
2022-07-06 15:13:43 +0300

    tries with params

--- lesson_2/test.py
+++ lesson_2/test.py
@@ -1,4 +1,4 @@
-from urllib import response
+from cgitb import text
 import requests
 import pprint
 from bs4 import BeautifulSoup
@@ -6,12 +6,12 @@ from soupsieve import select
 from transliterate import translit, get_available_language_codes
 
 headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
-text_translite=translit(input('write name of vacancy here:').replace(' ', '_'), 'ru', reversed=True)
-url = f'https://hh.ru/vacancies/{text_translite}'.replace("'", "").lower()
-print(url)
+text_translite=translit(input('write name of vacancy here:').replace(' ', '_').replace("'", "").lower(), 'ru', reversed=True)
+url = 'https://hh.ru/vacancy'
+params = {'text': text_translite}
 session = requests.Session()
-response = session.get(url, headers=headers)
-dom = BeautifulSoup(response.text, 'html.parser')
-vacancies = dom.select('span.g-user-content')
-print(vacancies)
-
+resp = session.get(url, headers=headers, params=params)
+print(resp)
+dom = BeautifulSoup(resp.content, 'html.parser')
+#vacancy = dom.find('vacancy-serp-item')
+#print(vacancy)
